88363f530b9b5ff80a31525ca60971674df7f002
Stefán Örn Hrafnsson <stefanorn92@gmail.com>
2015-01-15 10:48:43 +0100

    Fyrsta commit
    kláraði 6 episodes af þessum tuturial

---
+++ Assets/Scripts/ClickableTile.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using System.Collections;
+
+public class ClickableTile : MonoBehaviour {
+
+	public int tileX;
+	public int tileY;
+	public TileMap map;
+
+	void OnMouseUp() {
+		Debug.Log ("Click!");
+
+		map.GeneratePathTo(tileX, tileY);
+	}
+
+}
